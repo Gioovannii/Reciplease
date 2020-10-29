964a55214c830625bf586507f5b8c34e0057836b
Giovanni Gaffe <gaffejonathan@ymail.com>
2020-10-29 21:02:50 +0800

    extension to convert Any string to a data

---
+++ Reciplease/Extension/String+Data.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  Reciplease
+//
+//  Created by Giovanni Gaffé on 2020/10/29.
+//  Copyright © 2020 Jonathan Gaffé. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    var data: Data? {
+        guard let url = URL(string: self) else { return nil }
+        guard let data =  try? Data(contentsOf: url) else { return nil }
+        return data
+    }
+    
+}
