f7705cdd9b9a7ad9d5ff9234a5d9de7b761cdbc1
Giovanni Gaffe <gaffejonathan@ymail.com>
2020-10-30 19:40:30 +0800

    New file for extension of double

---
+++ Reciplease/Extension/Double+String.swift
@@ -0,0 +1,27 @@
+//
+//  Double+String.swift
+//  Reciplease
+//
+//  Created by Giovanni Gaffé on 2020/10/30.
+//  Copyright © 2020 Jonathan Gaffé. All rights reserved.
+//
+
+import Foundation
+extension Double {
+    func convert(minutes: Self) -> String {
+        let formater = NumberFormatter()
+        formater.maximumFractionDigits = 2
+        
+        guard minutes != 0 else { return "Unavailable" }
+        guard minutes > 59 else {
+            guard let minTrans = formater.string(from: NSNumber(value: minutes)) else { return ""}
+            return minTrans
+        }
+        
+        let result = minutes / 60
+        guard let resultForm = formater.string(from: NSNumber(value: result)) else { return String() }
+        return "\(resultForm) hours"
+    }
+}
+
+
