b0150965e4d02753ccf8788055ede749fd033a90
Giovanni Gaffe <gaffejonathan@ymail.com>
2020-10-31 20:35:21 +0800

    use representable

--- Reciplease/Controller/DescriptionController.swift
+++ Reciplease/Controller/DescriptionController.swift
@@ -15,8 +15,8 @@ final class DescriptionController: UIViewController {
     @IBOutlet weak var favoriteButton: UIBarButtonItem!
     
     var coreDataManager: CoreDataManager?
-    var recipe: Recipe?
-    var recipeEntity: RecipeEntity?
+//    var recipe: Recipe?
+//    var recipeEntity: RecipeEntity?
     var recipeRepresentable: RecipeRepresentable?
     var ingredients = [String]()
     
@@ -33,11 +33,14 @@ final class DescriptionController: UIViewController {
         guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
         coreDataManager = CoreDataManager(coreDataStack: appDelegate.coreDataStack)
 
-        guard let ingredientLines = recipe?.ingredientLines else { return }
+//        guard let ingredientLines = recipe?.ingredientLines else { return }
+        guard let ingredientLines = recipeRepresentable?.ingredientLines else { return }
         ingredients = ingredientLines
         
-        guard let recipe = recipe else { return }
-        guard let url = URL(string: recipe.image) else { return }
+//        guard let recipe = recipe else { return }
+        guard let recipe = recipeRepresentable else { return }
+
+        guard let url = URL(string: recipe.url) else { return }
         
         recipeImageView.sd_setImage(with: url, placeholderImage: UIImage(named: "cooking"),
                                     options: [], completed: nil)
@@ -55,21 +58,21 @@ final class DescriptionController: UIViewController {
     }
     
     @IBAction func getDirectionsButtonTapped(_ sender: UIButton) {
-        guard let shareAs = recipe?.shareAs else { return }
+        guard let shareAs = recipeRepresentable?.shareAs else { return }
         if let url = URL(string: shareAs) {
             UIApplication.shared.open(url)
         }
     }
     
     @IBAction func favoriteButtonTapped(_ sender: UIBarButtonItem) {
-        guard let recipe = recipe else { return }
+        guard let recipe = recipeRepresentable else { return }
         let recipeTitle = recipe.label
         guard let coreDataManager = coreDataManager else { return }
         guard let health = recipe.healthLabels.first else { return }
         
         switch coreDataManager.isRecipeRegistered(for: recipeTitle) {
         case false:
-            coreDataManager.createRecipe(title: recipe.label, health: health, time: "\(recipe.totalTime)", ingredients: recipe.ingredientLines, sourceUrl: recipe.url, image: recipe.image.data)
+//            coreDataManager.createRecipe(title: recipe.label, health: health, time: "\(recipe.totalTime)", ingredients: recipe.ingredientLines, sourceUrl: recipe.source, image: recipe.image.data)
             sender.image = UIImage(named: "fullHeart")
             
         case true:
@@ -98,7 +101,7 @@ extension DescriptionController: UITableViewDataSource {
     // MARK: - Header
     
     internal func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return  recipe?.label
+        return  recipeRepresentable?.label
     }
 }
 
