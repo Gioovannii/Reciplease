04ba4c1eebbdb50b6197a2a11ecd4155aa7edf2b
Giovanni Gaffe <gaffejonathan@ymail.com>
2020-11-02 16:02:53 +0800

    Remove of unused constant

--- Reciplease/Model/EdanamJSON.swift
+++ Reciplease/Model/EdanamJSON.swift
@@ -31,17 +31,14 @@ struct Recipe: Codable {
     let uri: String
     let label: String
     var image: String
-    let source: String
     let url: String
     let shareAs: String
-    let dietLabels, healthLabels, cautions, ingredientLines: [String]
+    let dietLabels, healthLabels, ingredientLines: [String]
     let totalTime: Int
 }
 
 struct RecipeRepresentable {
     var imageData: Data?
-    let url: String
-    let source: String
     let ingredients: [String]
     let label: String
     let totalTime: String
