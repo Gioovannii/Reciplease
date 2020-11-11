0f2ae32c3f8a27ea2fefea68541ac713e7bc1595
Giovanni Gaffe <gaffejonathan@ymail.com>
2020-11-11 18:18:54 +0800

    Set property we just mock to use coreDataManager

---
+++ RecipleaseTests/CoreDataTests/CoreDataManagerTests.swift
@@ -0,0 +1,19 @@
+//
+//  CoreDataManagerTests.swift
+//  RecipleaseTests
+//
+//  Created by Giovanni Gaffé on 2020/11/7.
+//  Copyright © 2020 Jonathan Gaffé. All rights reserved.
+//
+
+@testable import Reciplease
+import XCTest
+
+final class CoreDataManagerTests: XCTestCase {
+
+    // MARK: - Properties
+
+    var coreDataStack: MockCoreDataStack!
+    var coreDataManager: CoreDataManager!
+
+}
