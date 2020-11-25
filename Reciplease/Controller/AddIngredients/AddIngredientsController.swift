d008369a4387109bbe800f935eea86fdbb599b93
Giovanni Gaffe <gaffejonathan@ymail.com>
2020-11-25 18:39:22 +0800

    Remove service No use here because we have ViewModel change name Set data Source to the new fil dataSource then set him in viewDidLoad
    New Method added to set activity indicator and set him in view did Load

---
+++ Reciplease/Controller/AddIngredients/AddIngredientsController.swift
@@ -0,0 +1,79 @@
+//
+//  AddIngredientsController.swift
+//  Reciplease
+//
+//  Created by Giovanni Gaffé on 2020/8/24.
+//  Copyright © 2020 Jonathan Gaffé. All rights reserved.
+//
+
+import UIKit
+
+final class AddIngredientsController: UIViewController {
+    
+    
+    // TODO: - Instantiate controller with viewModel property from AddIngredientsViewModel
+    var viewModel: AddIngredientsViewModel?
+    
+    // MARK: - Properties
+    
+    private let request: RequestService = RequestService()
+    private var recipes = [Hit]()
+    private let dataSource = AddIngredientDataSource()
+    
+    
+    @IBOutlet private weak var ingredientTextField: UITextField!
+    @IBOutlet private weak var ingredientsTableView: UITableView!
+    @IBOutlet weak var searchForRecipesButton: UIButton!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        view.addGestureRecognizer(tap)
+        isSearching(result: true)
+        ingredientsTableView.dataSource = dataSource
+        viewModel = AddIngredientsViewModel()
+        bind()
+        viewModel?.viewDidLoad()
+    }
+    
+    // Change 
+    func bind() {
+        viewModel?.ingredientsList = { ingredients in
+            self.ingredients = ingredients
+        }
+    }
+    
+    @objc func dismissKeyboard() { view.endEditing(true) }
+    
+    @IBAction private func addIngredient(_ sender: UIButton) {
+        guard let str = ingredientTextField.text else { return }
+        let ingredients = str.trimmingCharacters(in: .whitespacesAndNewlines)
+        ingredientTextField.text = ""
+        viewModel?.addIngredient(name: ingredients)
+        ingredientsTableView.reloadData()
+        
+    }
+    
+    @IBAction private func clearIngredientsButton(_ sender: UIButton) {
+        viewModel?.clearAllIngredients()
+        ingredientsTableView.reloadData()
+    }
+    
+    @IBAction private func searchRecipesButton(_ sender: UIButton) { viewModel?.searchRecipes() }
+ 
+    func isSearching(result: Bool) {
+        switch result {
+        case true:
+            activityIndicator.isHidden = true
+            searchForRecipesButton.isHidden = false
+        case false:
+            activityIndicator.isHidden = false
+            activityIndicator.startAnimating()
+            searchForRecipesButton.isHidden = true
+        }
+    }
+}
+
