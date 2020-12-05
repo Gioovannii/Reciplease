f2dce8672e2b02aa770a7b4c95c47c25f9f396e4
Giovanni Gaffe <gaffejonathan@ymail.com>
2020-12-05 17:15:36 +0800

    instantiate datasource and view model

--- Reciplease/Controller/Recipe/RecipesViewController.swift
+++ Reciplease/Controller/Recipe/RecipesViewController.swift
@@ -11,9 +11,9 @@ import UIKit
 final class RecipesViewcontroller: UITableViewController {
     
     // MARK: - Properties
-
-    var recipes: [Hit]?
-    var recipe: Recipe?
+    
+    private let dataSource = RecipesDataSource()
+    var viewModel: RecipesViewModel?
     
     // MARK: - Life Cycle
 
@@ -47,27 +47,11 @@ final class RecipesViewcontroller: UITableViewController {
         if segue.identifier == Constant.toDescription {
             let vcDestination = segue.destination as! DescriptionController
             
-            guard let recipe = recipe else { return }
+            guard let recipe = viewModel?.recipe else { return }
             let timeConverted = Double().convert(minutes: Double(recipe.totalTime))
             
             let recipeRepresentable = RecipeRepresentable(label: recipe.label, imageData: recipe.image.data, healthLabels: recipe.healthLabels.first ?? "", ingredientLines: recipe.ingredientLines, totalTime: timeConverted, shareAs: recipe.shareAs)
             vcDestination.recipeRepresentable = recipeRepresentable
-        }
-    }
-    
-    override internal func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 260
-    }
-    
-    // MARK: - Animation cells
-
-    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-        let translationMouvement = CATransform3DTranslate(CATransform3DIdentity, 0, 100, 0)
-        cell.layer.transform = translationMouvement
-        cell.alpha = 0
-        UIView.animate(withDuration: 0.20) {
-            cell.layer.transform = CATransform3DIdentity
-            cell.alpha = 1
         }
     }
 }
