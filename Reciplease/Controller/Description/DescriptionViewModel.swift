07153c8719033ac464a0a314075e1b197bad3873
Giovanni Gaffe <gaffejonathan@ymail.com>
2020-12-06 09:56:17 +0800

    Create new file for description

---
+++ Reciplease/Controller/Description/DescriptionViewModel.swift
@@ -0,0 +1,29 @@
+//
+//  DescriptionViewModel.swift
+//  Reciplease
+//
+//  Created by Giovanni Gaffé on 2020/12/6.
+//  Copyright © 2020 Jonathan Gaffé. All rights reserved.
+//
+
+
+import Foundation
+
+final class DescriptionViewModel {
+    
+    // MARK: - Properties
+   
+    
+    // MARK: - Initializer
+
+   
+    // MARK: - Outputs
+   
+    
+    // MARK: - Inputs
+   
+    
+    // MARK: - Methods
+    
+   
+}
