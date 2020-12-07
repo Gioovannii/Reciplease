44a3786f48a29a4a96aa272302ce1dcf49ef5323
Giovanni Gaffe <gaffejonathan@ymail.com>
2020-12-07 10:52:29 +0800

    New file Data source for description

---
+++ Reciplease/Controller/Description/DescriptionDataSource.swift
@@ -0,0 +1,17 @@
+//
+//  DescriptionDataSource.swift
+//  Reciplease
+//
+//  Created by Giovanni Gaffé on 2020/12/6.
+//  Copyright © 2020 Jonathan Gaffé. All rights reserved.
+//
+
+import UIKit
+
+final class DescriptionDataSource: NSObject {
+
+}
+
+
+
+
