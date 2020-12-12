6e90e0c88346b1d801925222bd9bf61b6eaeddf8
Giovanni Gaffe <gaffejonathan@ymail.com>
2020-12-12 11:42:09 +0800

    Use didSet to set an object of recipeRepresentable instead of recipe

--- Reciplease/Controller/Recipes/RecipesViewModel.swift
+++ Reciplease/Controller/Recipes/RecipesViewModel.swift
@@ -12,7 +12,18 @@ final class RecipesViewModel {
     
     // MARK: - Properties
     
-    var recipe: Recipe?
+    var recipe: Recipe? {
+        didSet {
+            if let recipe = recipe {
+                let timeConverted = Double().convert(minutes: Double(recipe.totalTime))
+                
+                let recipeRepresentable = RecipeRepresentable(label: recipe.label, imageData: recipe.image.data, healthLabels: recipe.healthLabels.first ?? "", ingredientLines: recipe.ingredientLines, totalTime: "\(timeConverted)", shareAs: recipe.shareAs)
+                
+                recipeOutput?(recipeRepresentable)
+            }
+        }
+    }
+    
     var recipes = [Hit]() {
         didSet {
             recipesOutput?(recipes)
