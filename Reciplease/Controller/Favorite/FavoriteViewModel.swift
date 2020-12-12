2c91f24690038806fa77e23fbdc5abfaf5fa9ad9
Giovanni Gaffe <gaffejonathan@ymail.com>
2020-12-12 11:50:23 +0800

    Creation of viewModel file

---
+++ Reciplease/Controller/Favorite/FavoriteViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  FavoriteViewModel.swift
+//  Reciplease
+//
+//  Created by Giovanni Gaffé on 2020/12/9.
+//  Copyright © 2020 Jonathan Gaffé. All rights reserved.
+//
+
+import Foundation
+
+final class FavoriteViewModel {
+    
+}
