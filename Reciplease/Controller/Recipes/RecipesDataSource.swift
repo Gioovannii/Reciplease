9f63638c90f8b4881e95e8ee8a148dcd2c8758c2
Giovanni Gaffe <gaffejonathan@ymail.com>
2020-12-14 17:03:44 +0800

    indentation of code

--- Reciplease/Controller/Recipes/RecipesDataSource.swift
+++ Reciplease/Controller/Recipes/RecipesDataSource.swift
@@ -26,6 +26,7 @@ extension RecipesDataSource: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         recipes.count
     }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: Constant.reusableCell, for: indexPath) as? RecipeTableViewCell else {
             print("Error while loading cell")
@@ -34,6 +35,7 @@ extension RecipesDataSource: UITableViewDataSource {
         cell.recipe = recipes[indexPath.row].recipe
         return cell
     }
+    
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 260
     }
