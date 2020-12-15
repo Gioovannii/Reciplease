e3a60ba605b8feea113a33390ec05b528bf78f37
Giovanni Gaffe <gaffejonathan@ymail.com>
2020-12-15 10:59:22 +0800

    Set favorite viewModel with reactive property

--- Reciplease/Controller/Favorite/FavoriteViewModel.swift
+++ Reciplease/Controller/Favorite/FavoriteViewModel.swift
@@ -10,4 +10,38 @@ import Foundation
 
 final class FavoriteViewModel {
     
+    // MARK: - Properties
+    
+    var recipe: RecipeEntity? {
+        didSet {
+                let recipeRepresentable = RecipeRepresentable(label: recipe!.title ?? "", imageData: recipe!.image, healthLabels: recipe!.healthLabel ?? "", ingredientLines: recipe!.ingredients ?? [], totalTime: recipe!.time ?? "", shareAs: recipe!.shareAs ?? "")
+            recipeOutput?(recipeRepresentable)
+        }
+    }
+    
+    var coreDataManager: CoreDataManager
+    
+    // MARK: - Initializer
+
+    init(coreDataManager: CoreDataManager) {
+        self.coreDataManager = coreDataManager
+    }
+    
+    // MARK: - Outputs
+    
+    var recipeOutput: ((RecipeRepresentable) -> Void)?
+    var recipesOutput: (([RecipeEntity]) -> Void)?
+    
+    // MARK: - Inputs
+    
+    func viewDidLoad() {
+        recipesOutput?(coreDataManager.recipes)
+    }
+    
+    // MARK: - Method
+
+    func didSelectRecipe(at index: Int) {
+        self.recipe = coreDataManager.recipes[index]
+       
+    }
 }
