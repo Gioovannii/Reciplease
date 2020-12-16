12e81f8c22624397a1f1e22aa67645bdb515e479
Giovanni Gaffe <gaffejonathan@ymail.com>
2020-12-16 19:42:03 +0800

    creation Favorite view model

---
+++ RecipleaseTests/FavoriteTestCase/FavoriteViewModelTestCase.swift
@@ -0,0 +1,14 @@
+//
+//  FavoriteViewModelTestCase.swift
+//  RecipleaseTests
+//
+//  Created by Giovanni Gaffé on 2020/12/15.
+//  Copyright © 2020 Jonathan Gaffé. All rights reserved.
+//
+
+import XCTest
+
+class FavoriteViewModelTestCase: XCTestCase {
+
+
+}
