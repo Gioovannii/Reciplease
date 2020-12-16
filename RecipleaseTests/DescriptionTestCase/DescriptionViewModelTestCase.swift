f90c5988d94f08bf762cb9a74459a2f2c832d828
Giovanni Gaffe <gaffejonathan@ymail.com>
2020-12-16 19:41:14 +0800

    creation Description view model

---
+++ RecipleaseTests/DescriptionTestCase/DescriptionViewModelTestCase.swift
@@ -0,0 +1,14 @@
+//
+//  DescriptionViewModelTestCase.swift
+//  RecipleaseTests
+//
+//  Created by Giovanni Gaffé on 2020/12/15.
+//  Copyright © 2020 Jonathan Gaffé. All rights reserved.
+//
+
+import XCTest
+
+class DescriptionViewModelTestCase: XCTestCase {
+
+  
+}
