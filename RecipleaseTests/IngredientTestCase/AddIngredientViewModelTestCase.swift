f2671cbc2f672e0d44328b6351403e54101e0ce3
Giovanni Gaffe <gaffejonathan@ymail.com>
2020-12-16 19:44:03 +0800

    Creation AddIngredient view model

---
+++ RecipleaseTests/IngredientTestCase/AddIngredientViewModelTestCase.swift
@@ -0,0 +1,14 @@
+//
+//  AddIngredientViewModelTestCase.swift
+//  RecipleaseTests
+//
+//  Created by Giovanni Gaffé on 2020/12/15.
+//  Copyright © 2020 Jonathan Gaffé. All rights reserved.
+//
+
+import XCTest
+@testable import Reciplease
+
+class AddIngredientViewModelTestCase: XCTestCase {
+ 
+}
