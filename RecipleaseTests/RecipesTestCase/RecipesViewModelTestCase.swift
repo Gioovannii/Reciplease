92d7db5c47f0fba77d38a12d69d2b50178eca7e2
Giovanni Gaffe <gaffejonathan@ymail.com>
2020-12-24 13:12:39 +0800

    Add testable import Reciplease and set an instance of recipe

--- RecipleaseTests/RecipesTestCase/RecipesViewModelTestCase.swift
+++ RecipleaseTests/RecipesTestCase/RecipesViewModelTestCase.swift
@@ -7,7 +7,16 @@
 //
 
 import XCTest
+@testable import Reciplease
 
 class RecipesViewModelTestCase: XCTestCase {
 
+//    var recipe = Recipe(label: "", image: <#T##String#>, url: <#T##String#>, healthLabels: <#T##[String]#>, ingredientLines: <#T##[String]#>, totalTime: <#T##Int#>, shareAs: <#T##String#>)
+    var recipes = [Hit]()
+
+    func testDidSetRecipe() {
+        let viewModel = RecipesViewModel(recipes: self.recipes)
+        
+        viewModel.recipes = recipes
+    }
 }
