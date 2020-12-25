0ff7645d681d38467233209f34f0344f4094cf5e
Giovanni Gaffe <gaffejonathan@ymail.com>
2020-12-25 11:48:58 +0800

    Test view model when ingredient is added ingredientList should be update

--- RecipleaseTests/IngredientTestCase/AddIngredientViewModelTestCase.swift
+++ RecipleaseTests/IngredientTestCase/AddIngredientViewModelTestCase.swift
@@ -10,5 +10,23 @@ import XCTest
 @testable import Reciplease
 
 class AddIngredientViewModelTestCase: XCTestCase {
+
+    // MARK: - Ingredient List
  
+    func testAddIngredient_WhenIngredientsIsAdded_ThenShouldReturnListTransform() {
+        let session = MockEdanamSession(fakeResponse: FakeResponse(response: FakeResponseData.responseOK))
+        let requestService = RequestService(session: session)
+        let viewModel = AddIngredientsViewModel(service: requestService)
+        
+        let expectation = XCTestExpectation(description: "Wait for queue change.")
+
+        let expectedResult = ["- Tomatoe"]
+        viewModel.ingredientsList = { ingredients in
+            XCTAssert(expectedResult == ingredients)
+            expectation.fulfill()
+        }
+        viewModel.addIngredient(name: "Tomatoe")
+        wait(for: [expectation], timeout: 0.01)
+        
+    }
 }
