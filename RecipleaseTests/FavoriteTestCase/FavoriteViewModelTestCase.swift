f6335f72b61011a9e9fb4e075b27ebea6755fb3b
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-01-08 08:54:54 +0800

    TestWhen we add recipeToCoreDataManager then we should see this on by access his properties

--- RecipleaseTests/FavoriteTestCase/FavoriteViewModelTestCase.swift
+++ RecipleaseTests/FavoriteTestCase/FavoriteViewModelTestCase.swift
@@ -29,5 +29,23 @@ class FavoriteViewModelTestCase: XCTestCase {
         coreDataManager = nil
     }
     
+    func testRecipeDidSet_WhenSetANewRecipeInCoreDataManager_ThenWeCanAssertTheActualRecipeIsExpectedResult() {
+        let viewModel = FavoriteViewModel(coreDataManager: coreDataManager)
+        let expectaction = XCTestExpectation(description: "Wait for queue change")
+        
+        let expectedResult = "Lemon Confit"
+        viewModel.recipeOutput = { recipe in
+            XCTAssert(expectedResult == recipe.label)
+            expectaction.fulfill()
+            
+        }
+        viewModel.recipeOutput?(recipeRepresentable)
+        viewModel.coreDataManager.createRecipe(title: "Lemon Confit", health: "Vegan", time: "0", ingredients: ["Kosher salt to cover (about 2 pounds/900 grams)","1/2 to 1 cup water or lemon juice (125 to 250 ml)","6 lemons, scrubbed and halved crosswise"], shareAs: "http://www.edamam.com/recipe/lemon-confit-2fb391cceeec3d82920a2035f1849d72/lemon",
+            image: "https://www.edamam.com/web-img/d32/d32b4dc2e7bd9d4d1a24bbced0c89143.jpg".data)
+        viewModel.recipe = coreDataManager.recipes[0]
+        wait(for: [expectaction], timeout: 0.01)
+        
+    }
+    
     
 }
