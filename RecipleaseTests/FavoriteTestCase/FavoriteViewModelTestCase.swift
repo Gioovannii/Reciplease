2c162a82b160158e7fd6705ec884a58d1b5c451c
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-01-08 08:52:47 +0800

    Set a new recipeRepresentable

--- RecipleaseTests/FavoriteTestCase/FavoriteViewModelTestCase.swift
+++ RecipleaseTests/FavoriteTestCase/FavoriteViewModelTestCase.swift
@@ -10,20 +10,11 @@ import XCTest
 @testable import Reciplease
 
 class FavoriteViewModelTestCase: XCTestCase {
+
+    var recipeRepresentable = RecipeRepresentable(label: "Lemon Confit", imageData: "https://www.edamam.com/web-img/d32/d32b4dc2e7bd9d4d1a24bbced0c89143.jpg".data, healthLabels: "Vegan", ingredientLines: ["Kosher salt to cover (about 2 pounds/900 grams)","1/2 to 1 cup water or lemon juice (125 to 250 ml)","6 lemons, scrubbed and halved crosswise"], totalTime: "0", shareAs: "http://www.edamam.com/recipe/lemon-confit-2fb391cceeec3d82920a2035f1849d72/lemon")
+    
     
     var coreDataStack: CoreDataStack!
     var coreDataManager: CoreDataManager!
-    
-    
-    override func setUp() {
-        coreDataStack = MockCoreDataStack()
-        coreDataManager = CoreDataManager(coreDataStack: coreDataStack)
-    }
-    
-    override func tearDown() {
-        coreDataStack = nil
-        coreDataManager = nil
-    }
-    
-    
+
 }
