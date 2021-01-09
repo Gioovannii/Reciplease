4b0f8601f840274dae67e060f9c7d13e829981e0
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-01-09 17:11:02 +0800

    Clean code

--- RecipleaseTests/DescriptionTestCase/DescriptionViewModelTestCase.swift
+++ RecipleaseTests/DescriptionTestCase/DescriptionViewModelTestCase.swift
@@ -18,7 +18,6 @@ class DescriptionViewModelTestCase: XCTestCase {
 
     var recipeRepresentable = RecipeRepresentable(label: "Lemon Confit", imageData: "https://www.edamam.com/web-img/d32/d32b4dc2e7bd9d4d1a24bbced0c89143.jpg".data, healthLabels: "Vegan", ingredientLines: ["Kosher salt to cover (about 2 pounds/900 grams)","1/2 to 1 cup water or lemon juice (125 to 250 ml)","6 lemons, scrubbed and halved crosswise"], totalTime: "0", shareAs: "http://www.edamam.com/recipe/lemon-confit-2fb391cceeec3d82920a2035f1849d72/lemon")
 
-  
     override func setUp() {
         super.setUp()
         coreDataStack = MockCoreDataStack()
