4bca3262ff7a73cf5257e8f65bcfa032d1329f2a
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-01-09 17:19:04 +0800

    remove programmatic code replace by method create above

--- RecipleaseTests/DescriptionTestCase/DescriptionViewModelTestCase.swift
+++ RecipleaseTests/DescriptionTestCase/DescriptionViewModelTestCase.swift
@@ -75,8 +75,8 @@ class DescriptionViewModelTestCase: XCTestCase {
             }
             expectation.fulfill()
         }
-        viewModel.coreDataManager.createRecipe(title: "Lemon Confit", health: "Vegan", time: "0", ingredients: ["Kosher salt to cover (about 2 pounds/900 grams)","1/2 to 1 cup water or lemon juice (125 to 250 ml)","6 lemons, scrubbed and halved crosswise"], shareAs: "http://www.edamam.com/recipe/lemon-confit-2fb391cceeec3d82920a2035f1849d72/lemon",
-            image: "https://www.edamam.com/web-img/d32/d32b4dc2e7bd9d4d1a24bbced0c89143.jpg".data)
+      
+        createRecipe()
         viewModel.favoriteLoad()
         wait(for: [expectation], timeout: 0.01)
     }
