b4a0e75b8502da1c01b206eca3278c7857d8abce
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-01-09 17:13:16 +0800

    Set method for create recipe

--- RecipleaseTests/DescriptionTestCase/DescriptionViewModelTestCase.swift
+++ RecipleaseTests/DescriptionTestCase/DescriptionViewModelTestCase.swift
@@ -30,6 +30,13 @@ class DescriptionViewModelTestCase: XCTestCase {
         coreDataManager = nil
     }
     
+    func createRecipe() {
+        let viewModel = DescriptionViewModel(recipe: recipeRepresentable, coreDataManager: coreDataManager)
+
+        viewModel.coreDataManager.createRecipe(title: "Lemon Confit", health: "Vegan", time: "0", ingredients: ["Kosher salt to cover (about 2 pounds/900 grams)","1/2 to 1 cup water or lemon juice (125 to 250 ml)","6 lemons, scrubbed and halved crosswise"], shareAs: "http://www.edamam.com/recipe/lemon-confit-2fb391cceeec3d82920a2035f1849d72/lemon",
+            image: "https://www.edamam.com/web-img/d32/d32b4dc2e7bd9d4d1a24bbced0c89143.jpg".data)
+    }
+    
     func testDidSetRecipe_WhenWeChangeValue_ThenWeEnterInside() {
         
         let viewModel = DescriptionViewModel(recipe: recipeRepresentable, coreDataManager: coreDataManager)
